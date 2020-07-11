388d6c4f76be9c0423304bb81a153f487dbb2806
yanqiangmiffy <1185918903@qq.com>
2020-07-11 23:22:35 +0800

    lgb线上0.67

--- bn_cnn.py
+++ bn_cnn.py
@@ -1,10 +1,9 @@
 from scipy.signal import resample
 import pandas as pd
-import tensorflow as tf
 import numpy as np
 import pandas as pd
 from keras import Model
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.utils import to_categorical
 from scipy.signal import resample
 from sklearn.metrics import accuracy_score
@@ -16,57 +15,33 @@ from utils import acc_combo
 train = pd.read_csv('data/sensor_train.csv')
 test = pd.read_csv('data/sensor_test.csv')
 train_size = len(train)
+
 data = pd.concat([train, test], sort=False)
+
 sub = pd.read_csv('data/提交结果示例.csv')
 y = train.groupby('fragment_id')['behavior_id'].min()
 
-data['acc'] = (data.acc_x ** 2 + data.acc_y ** 2 + data.acc_z ** 2) ** .5
-data['accg'] = (data.acc_xg ** 2 + data.acc_yg ** 2 + data.acc_zg ** 2) ** .5
+train['mod'] = (train.acc_x ** 2 + train.acc_y ** 2 + train.acc_z ** 2) ** .5
+train['modg'] = (train.acc_xg ** 2 + train.acc_yg ** 2 + train.acc_zg ** 2) ** .5
+test['mod'] = (test.acc_x ** 2 + test.acc_y ** 2 + test.acc_z ** 2) ** .5
+test['modg'] = (test.acc_xg ** 2 + test.acc_yg ** 2 + test.acc_zg ** 2) ** .5
 
-data['acc1'] = (data['acc_x'] ** 2 + data['acc_y'] ** 2) ** 0.5
-data['accg1'] = (data['acc_xg'] ** 2 + data['acc_yg'] ** 2) ** 0.5
-
-data['acc2'] = (data['acc_x'] ** 2 + data['acc_z'] ** 2) ** 0.5
-data['accg2'] = (data['acc_xg'] ** 2 + data['acc_zg'] ** 2) ** 0.5
-
-#     data['acc3'] = (data['acc_y'] ** 2 + data['acc_z'] ** 2) ** 0.5
-#     data['accg3'] = (data['acc_yg'] ** 2 + data['acc_zg'] ** 2) ** 0.5  # y - z系列 under 4%%
-
-
-data['acc_sub'] = ((data['acc_xg'] - data['acc_x']) ** 2 + (data['acc_yg'] - data['acc_y']) ** 2 + (
-        data['acc_zg'] - data['acc_z']) ** 2) ** 0.5
-data['acc_sub1'] = ((data['acc_xg'] - data['acc_x']) ** 2 + (data['acc_yg'] - data['acc_y']) ** 2) ** 0.5
-data['acc_sub2'] = ((data['acc_xg'] - data['acc_x']) ** 2 + (data['acc_zg'] - data['acc_z']) ** 2) ** 0.5
-#     data['acc_sub3'] = ((data['acc_yg'] - data['acc_y']) ** 2 + (data['acc_zg'] - data['acc_z'])**2) ** 0.5
-
-
-data['accxg_diff_accx'] = data['acc_xg'] - data['acc_x']
-data['accyg_diff_accy'] = data['acc_yg'] - data['acc_y']
-data['acczg_diff_accz'] = data['acc_zg'] - data['acc_z']
-
-# abs
-
-train, test = data[:train_size], data[train_size:]
-
-no_fea = ['fragment_id', 'behavior_id', 'time_point', 'inv_fragment_id', 'inv_behavior_id', 'inv_time_point']
-use_fea = [fea for fea in train.columns if fea not in no_fea]
-print("use_fea", use_fea)
-fea_size = len(use_fea)
-
-x = np.zeros((7292, 60, fea_size, 1))
-t = np.zeros((7500, 60, fea_size, 1))
+x = np.zeros((7292, 60, 8, 1))
+t = np.zeros((7500, 60, 8, 1))
 for i in tqdm(range(7292)):
     tmp = train[train.fragment_id == i][:60]
-    x[i, :, :, 0] = resample(tmp[use_fea], 60, np.array(tmp.time_point))[0]
+    x[i, :, :, 0] = resample(tmp.drop(['fragment_id', 'time_point', 'behavior_id'],
+                                      axis=1), 60, np.array(tmp.time_point))[0]
 for i in tqdm(range(7500)):
     tmp = test[test.fragment_id == i][:60]
-    t[i, :, :, 0] = resample(tmp[use_fea], 60, np.array(tmp.time_point))[0]
+    t[i, :, :, 0] = resample(tmp.drop(['fragment_id', 'time_point'],
+                                      axis=1), 60, np.array(tmp.time_point))[0]
 
 kfold = StratifiedKFold(5, shuffle=True)
 
 
 def Net():
-    input = Input(shape=(60, fea_size, 1))
+    input = Input(shape=(60, 8, 1))
     X = Conv2D(filters=64,
                kernel_size=(3, 3),
                activation='relu',
@@ -79,7 +54,6 @@ def Net():
     X = BatchNormalization()(X)
 
     X = MaxPooling2D()(X)
-    X = AveragePooling2D()(X)
     X = Dropout(0.2)(X)
     X = Conv2D(filters=256,
                kernel_size=(3, 3),
@@ -115,26 +89,25 @@ for fold, (xx, yy) in enumerate(kfold.split(x, y)):
                                 verbose=1,
                                 mode='max',
                                 factor=0.5,
-                                patience=15)
+                                patience=8)
     early_stopping = EarlyStopping(monitor='val_acc',
                                    verbose=1,
                                    mode='max',
-                                   patience=20)
+                                   patience=18)
     checkpoint = ModelCheckpoint(f'models/fold{fold}.h5',
                                  monitor='val_acc',
                                  verbose=0,
                                  mode='max',
                                  save_best_only=True)
-
-    csv_logger = CSVLogger('logs/log.csv', separator=',', append=True)
     model.fit(x[xx], y_[xx],
               epochs=500,
-              batch_size=64,
+              batch_size=256,
               verbose=2,
               shuffle=True,
               validation_data=(x[yy], y_[yy]),
-              callbacks=[plateau, early_stopping, checkpoint, csv_logger])
+              callbacks=[plateau, early_stopping, checkpoint])
     model.load_weights(f'models/fold{fold}.h5')
+
     proba_x = model.predict(x[yy], verbose=0, batch_size=1024)
     proba_t += model.predict(t, verbose=0, batch_size=1024) / 5.
 
@@ -148,4 +121,4 @@ for fold, (xx, yy) in enumerate(kfold.split(x, y)):
 print("5kflod mean acc score:{}".format(np.mean(acc_scores)))
 print("5kflod mean combo score:{}".format(np.mean(combo_scores)))
 sub.behavior_id = np.argmax(proba_t, axis=1)
-sub.to_csv('result/submit_acc{}_combo{}.csv'.format(np.mean(acc_scores), np.mean(combo_scores)), index=False)
+sub.to_csv('result/submit.csv', index=False)
